47a15dad7acdc548a3aef6ae7f75be1137e6505a
Jeremy Karlsson <jeremy.karlsson@tidal.com>
2018-12-03 17:18:06 +0100

    Add close button to <app-header>

--- src/js/components/app-header/app-header.js
+++ src/js/components/app-header/app-header.js
@@ -4,6 +4,12 @@ class AppHeader extends Web.Component {
   constructor () {
     super(import.meta.url);
   }
+
+  componentDidMount () {
+    this.$('button').addEventListener('click', () => {
+      window.close();
+    });
+  }
 }
 
 export default Web.registerComponent(AppHeader);
