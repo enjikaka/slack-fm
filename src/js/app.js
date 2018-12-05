9c3c112ab6268c1f431707d47dc062d461e3ef1a
Jeremy Karlsson <jeremy.karlsson@tidal.com>
2018-12-05 18:55:39 +0100

    Add sign in screen

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,24 @@
 import './slack.js';
 import './components/app-header/app-header.js';
+import './components/sign-in/sign-in.js';
 import './components/last-fm-box/last-fm-box.js';
 import './components/last-fm-user/last-fm-user.js';
 import './components/last-fm-now-playing/last-fm-now-playing.js';
 import './components/last-fm-username-chooser/last-fm-username-chooser.js';
 import './components/slack-box/slack-box.js';
+
+const lastFMUser = localStorage.getItem('last-fm-username');
+
+if (lastFMUser) {
+  document.body.querySelector('main').appendChild(
+    document.createElement('last-fm-box')
+  );
+
+  document.body.querySelector('main').appendChild(
+    document.createElement('slack-box')
+  );
+} else {
+  document.body.querySelector('main').appendChild(
+    document.createElement('sign-in')
+  );
+}
---
+++ src/js/components/sign-in/sign-in.js
@@ -0,0 +1,33 @@
+import * as Web from '../web.js';
+
+class SignIn extends Web.Component {
+  constructor () {
+    super(import.meta.url);
+  }
+
+  componentDidMount () {
+    const input = this.$('#last-fm-username');
+    const button = this.$('#last-fm-continue-button');
+
+    button.addEventListener('click', event => {
+      this.classList.add('last-fm-load');
+      localStorage.setItem('last-fm-username', input.value);
+
+      document.body.querySelector('main').appendChild(
+        document.createElement('last-fm-box')
+      );
+
+      document.body.querySelector('main').appendChild(
+        document.createElement('slack-box')
+      );
+
+      setTimeout(() => {
+        window.requestIdleCallback(() => {
+          this.classList.add('last-fm-done');
+        });
+      }, 2000);
+    });
+  }
+}
+
+export default Web.registerComponent(SignIn);
